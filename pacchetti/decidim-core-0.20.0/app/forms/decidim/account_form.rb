d0eb896dfc651a50fec53101aaed80cf9287efaf
mariosabatino <eustema@github.it>
2021-12-17 17:40:05 +0100

    nascosti tab account e aggiunta stellina admin

--- pacchetti/decidim-core-0.20.0/app/forms/decidim/account_form.rb
+++ pacchetti/decidim-core-0.20.0/app/forms/decidim/account_form.rb
@@ -18,7 +18,8 @@ module Decidim
 
     validates :name, presence: true
     validates :email, presence: true, 'valid_email_2/email': { disposable: true }
-    validates :nickname, presence: true, format: /\A[\w\-]+\z/
+    #validates :nickname, presence: true, format: /\A[\w\-]+\z/
+    validates :nickname, presence: true, format: /\A[\w\_]+\z/
 
     validates :nickname, length: { maximum: Decidim::User.nickname_max_length, allow_blank: true }
     validates :password, confirmation: true
