d01d88dc13a60c21e77aed0a619036308f90bde9
mariosabatino <eustema@github.it>
2021-12-17 15:35:57 +0100

    Rimozione del link dall'utente menzionato nelle email generate da un nuovo commento

--- pacchetti/decidim-comments-0.20.0/app/events/decidim/comments/reply_created_event.rb
+++ pacchetti/decidim-comments-0.20.0/app/events/decidim/comments/reply_created_event.rb
@@ -3,7 +3,13 @@
 module Decidim
   module Comments
     class ReplyCreatedEvent < Decidim::Events::SimpleEvent
+
       include Decidim::Comments::CommentEvent
+
+      def resource_text
+        ActionView::Base.full_sanitizer.sanitize(comment.formatted_body, :tags => %w(href))
+      end
+
     end
   end
 end
